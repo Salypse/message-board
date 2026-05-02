787c740f95d8392044e8e9c17417d92fa6763f8c
Salypse <84153483+Salypse@users.noreply.github.com>
2026-05-02 11:45:20 -0500

    Update index to display all messages with their text, user, and date added

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const indexRouter = express.Router();
 
+const messages = [
+   {
+      text: "Hello, Viltrum!",
+      user: "Thragg",
+      added: new Date()
+   },
+   {
+      text: "Hello, Luthadel!",
+      user: "Elend Venture",
+      added: new Date()
+   }
+]
+
 indexRouter.get("/", (req, res) => {
-   res.render("index", { message: "Hello, World!" });
+   res.render("index", { messages: messages});
 });
 
 module.exports = indexRouter;
