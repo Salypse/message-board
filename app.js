8b11bcef1fea06c00236ee79258783c9d3b7007d
Salypse <84153483+Salypse@users.noreply.github.com>
2026-05-07 11:14:40 -0500

    Create 404 error page

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require("express");
 const path = require("node:path");
 const app = express();
 
-const {indexRouter, messages} = require("./routes/indexRouter");
+const { indexRouter, messages } = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
-const detailsRouter = require("./routes/detailsRouter")
+const detailsRouter = require("./routes/detailsRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -15,15 +15,17 @@ const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use("/", indexRouter);
-app.use("/new", newRouter)
-app.use("/details", detailsRouter)
+app.use("/new", newRouter);
+app.use("/details", detailsRouter);
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
-   if (error) {
-      throw error;
-   }
-   console.log(`Listening on Port: ${PORT}`);
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on Port: ${PORT}`);
 });
