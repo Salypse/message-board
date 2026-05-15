2219c09e128d438c24e1a9b57020293547ebc2bd
Salypse <84153483+Salypse@users.noreply.github.com>
2026-05-15 11:29:57 -0500

    Add validation to form on /new page

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,13 +1,36 @@
 const db = require("../db/queries");
 const formatDate = require("../utils/formatDate");
+const { body, validationResult } = require("express-validator");
+
+const validateMessage = [
+  body("user")
+    .trim()
+    .notEmpty()
+    .withMessage("Username can not be empty.")
+    .isAlpha()
+    .withMessage("Username must only contain alphabet letters."),
+  body("message")
+    .trim()
+    .notEmpty()
+    .withMessage("Message can not be empty.")
+    .isLength({ max: 250 })
+    .withMessage("Message must be less than 250 characters."),
+];
 
 exports.newGet = (req, res, next) => {
   res.render("form");
 };
 
-exports.newPost = (req, res, next) => {
-  const { user, message } = req.body;
-  db.postNewMessage(user, message, formatDate(new Date()));
+exports.newPost = [
+  validateMessage,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("form", { errors: errors.array() });
+    }
+    const { user, message } = req.body;
+    db.postNewMessage(user, message, formatDate(new Date()));
 
-  res.redirect("/");
-};
+    res.redirect("/");
+  },
+];
